70b1cb2fa2d6fe17aad5c7434f07d93ca6a94d4b
zheng <esz759486@gmail.com>
2018-07-18 17:41:41 +0800

    lightbox內的img尺寸調整完成
    Signed-off-by: zheng <esz759486@gmail.com>

--- js/product.js
+++ js/product.js
@@ -2,16 +2,22 @@ const app = new Vue({
     el: '#app',
     data(){
         return{
-            show:false
+            show:false,
+            images:[],
+            lightboxImage:''
         }
     },
     mounted() {
-       console.log("Qqq"); 
+        //TODO:編寫AJAX取得圖片
+        let self = this;
+        self.images.push('images/david-sola-530108-unsplash.jpg',
+        'images/alex-kalligas-460302-unsplash.jpg');
     },
     methods:{
-        showDialog(){
+        showLightbox(imagesKey){
             let self = this;
             self.show = !self.show;
+            self.lightboxImage = self.images[imagesKey];
             console.log(self.show);
         }
     }
